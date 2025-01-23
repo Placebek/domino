0074d3c29342d77180290d8b46326c8bb02b5b0f
Placebek <goblins.from.mars2003@gmail.com>
2025-01-23 13:42:17 +0500

    Add VideoChannel component and update routing for video calls

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Login } from './Login.vue'
 export { default as Home } from './Home.vue'
+export { default as VideoC } from './VideoChannel.vue'
--- src/router/auth.js
+++ src/router/auth.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth.store'
 import { Login } from '../components/pages'
 import { Home } from '../components/pages'
+import { VideoC } from '../components/pages'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: 'active',
   routes: [
     { path: '/', component: Home },
+    { path: '/video', component: VideoC },
     { path: '/login', component: Login },
   ],
 })
