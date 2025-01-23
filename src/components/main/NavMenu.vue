5ab48c8d7127752bb55192bf3849e9f6ce3256b4
Placebek <goblins.from.mars2003@gmail.com>
2025-01-24 01:49:51 +0500

    Refactor NavMenu component; redesign layout with an arc menu, enhance styling, and maintain routing functionality.

--- src/components/main/NavMenu.vue
+++ src/components/main/NavMenu.vue
@@ -1,3 +1,21 @@
+<template>
+  <div class="fixed bottom-0 w-full h-[7vh] z-50">
+    <!-- Создаем дугу -->
+    <div class="arc-menu bg-blue-200 relative">
+      <div
+        class="flex items-center justify-center absolute inset-x-0 top-1/2 transform -translate-y-1/2"
+      >
+        <RouterLink to="/home">
+          <DominoHomeSVG customClass="w-[14vw] h-[5vh]" />
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { useAuthStore } from '../../stores/auth.store.js'
@@ -5,33 +23,25 @@ import DominoHomeSVG from '../UI/svg/DominoHomeSVG.vue'
 const authStore = useAuthStore()
 </script>
 
-<script>
-export default {
-  components: {
-    DominoHomeSVG,
-  },
+<style scoped>
+.arc-menu {
+  width: 100%; /* Полная ширина */
+  height: 100%; /* Высота контейнера */
+  background-color: #60a5fa; /* Цвет фона */
+  border-top-left-radius: 20px; /* Закругление слева */
+  border-top-right-radius: 20px; /* Закругление справа */
+  height: 8vh; /* Высота дуги */
+  clip-path: ellipse(100% 100% at 50% 100%); /* Формируем дугу */
+  position: relative;
+  overflow: hidden; /* Скрываем элементы за пределами дуги */
+  box-shadow: 0 -4px 8px rgba(0, 0, 0, 0.2); /* Тень снизу */
 }
-</script>
 
-<template>
-  <div class="bg-blue-200 h-[5vh] w-full fixed bottom-0 z-50">
-    <div
-      class="text-white text-[2vh] font-semibold ml-[3vw] cursor-pointer hover:text-gray-300 flex items-center absolute mt-2"
-    >
-      <RouterLink to="/home">
-        <DominoHomeSVG customClass="w-[12vw] h-[4vh]" />
-      </RouterLink>
-      <div class="ml-2">Дома</div>
-    </div>
-    <!-- <div class="flex justify-end gap-x-5 mr-[2vw]">
-      <a
-        @click="authStore.logout()"
-        class="font-bold text-[1.3vw] mt-1 text-white hover:text-gray-200 cursor-pointer"
-        >Выйти</a
-      >
-    </div> -->
-  </div>
-  <div>
-    <RouterView />
-  </div>
-</template>
+.arc-menu .router-link {
+  color: white;
+}
+
+.arc-menu .router-link:hover {
+  color: #ddd; /* Цвет при наведении */
+}
+</style>
