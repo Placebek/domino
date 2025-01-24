af1e077227e7b8d3b301966b0b8c8114228c0326
Placebek <goblins.from.mars2003@gmail.com>
2025-01-24 19:42:17 +0500

    Add filter options to Home component; implement city, district, and street selection with search button.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -199,9 +199,72 @@ const product = ref([
 </script>
 
 <template>
-  <div class="">
-    <div class="flex justify-center items-center w-[30vw] h-[10vh]">
+  <div class="flex flex-col">
+    <div class="w-[30vw] h-[10vh]">
       <img :src="logo" alt="" />
+    </div>
+    <div class="ml-[2vw] bg-blue-200 p-2 mt-[2vh] w-[96vw] mr-[2vw] rounded-md relative">
+      <div>
+        Фильтрлер:
+        <div class="flex flex-row">
+          <div class="w-[40vw]">
+            Қаланы таңдаңыз:
+            <select class="w-[30vw]">
+              <option>Алматы</option>
+              <option>Астана</option>
+              <option>Алматы</option>
+              <option>Алматы</option>
+            </select>
+          </div>
+          <div>
+            <div>Район:</div>
+
+            <select class="w-[30vw]">
+              <option>Алатау</option>
+              <option>Алатау</option>
+              <option>Алатау</option>
+              <option>Алатау</option>
+            </select>
+          </div>
+        </div>
+        <div class="flex flex-row gap-x-[5vw]">
+          <div>
+            <div>Улица:</div>
+            <select class="w-[30vw]">
+              <option>Абая</option>
+              <option>Абая</option>
+              <option>Абая</option>
+              <option>Абая</option>
+            </select>
+          </div>
+          <button
+            class="bg-blue-500 text-white rounded-lg h-[4vh] px-[3vw] mt-[2vh] absolute right-[10vw]"
+          >
+            Іздеу
+          </button>
+          <div
+            class="text-blue-500 cursor-pointer mt-[2vh] absolute top-0 right-2 underline font-bold flex flex-row"
+          >
+            <div>Көбірек</div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="text-3xl font-bold text-center">Дома</div>
