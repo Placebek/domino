cc03a6efd4d97e958b545ec09e1f3c056351dab0
sakenzi <ermekulysaken8@gmail.com>
2025-01-29 14:50:11 +0600

    created fulltextsearch

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 celery -A app.api.search.celery.tasks worker --loglevel=info
+
+The trigger of postgresql
+
+CREATE OR REPLACE FUNCTION update_search_vector()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.search_vector := to_tsvector('russian', 
+    CONCAT_WS(' ', 
+              regexp_replace(NEW.name, '[^A-Za-zА-Яа-я0-9 ]', '', 'g'),
+              regexp_replace(NEW.description, '[^A-Za-zА-Яа-я0-9 ]', '', 'g'),
+              regexp_replace(NEW.house_number, '[^A-Za-zА-Яа-я0-9 ]', '', 'g')
+    )
+  );
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_search_vector
+BEFORE INSERT OR UPDATE ON houses
+FOR EACH ROW
+EXECUTE FUNCTION update_search_vector();
+
+CREATE TRIGGER trg_update_search_vector
+BEFORE INSERT OR UPDATE ON houses
+FOR EACH ROW
+EXECUTE FUNCTION update_search_vector();
--- app/api/search/celery/tasks.py
+++ app/api/search/celery/tasks.py
@@ -1,26 +1,26 @@
-from celery import Celery
-from sqlalchemy.future import select
-from sqlalchemy import func
-import asyncio
-from database.db import async_session_factory
-from model.model import House
+# from celery import Celery
+# from sqlalchemy.future import select
+# from sqlalchemy import func
+# import asyncio
+# from database.db import async_session_factory
+# from model.model import House
 
-app = Celery("tasks", broker="redis://localhost:6379/0")
+# app = Celery("tasks", broker="redis://localhost:6379/0")
 
-@app.task
-def update_search_vector():
-    asyncio.run(update_task())
+# @app.task
+# def update_search_vector():
+#     asyncio.run(update_task())
 
-async def update_task():
-    async with async_session_factory() as db:
-        stmt = select(House)
-        result = await db.execute(stmt)
-        houses = result.scalars().all()
+# async def update_task():
+#     async with async_session_factory() as db:
+#         stmt = select(House)
+#         result = await db.execute(stmt)
+#         houses = result.scalars().all()
 
-        for house in houses:
-            house.search_vector = func.to_tsvector(
-                'russian',  
-                f"{house.name} {house.description} {house.house_number}"
-            )
+#         for house in houses:
+#             house.search_vector = func.to_tsvector(
+#                 'russian',  
+#                 f"{house.name} {house.description} {house.house_number}"
+#             )
 
-        await db.commit()
+#         await db.commit()
